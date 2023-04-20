5cd79e7eb59386d2e529f3e567ee8d5800c64598
c-daly <chris@cdaly.me>
2023-04-20 18:52:33 -0400

    config file added

---
+++ config.h
@@ -0,0 +1,10 @@
+// File system location for server (ex: ./filesystem) - don't allow actions outside this area
+// server ip, port
+// message size?
+#ifndef CONFIG_H
+#define CONFIG_H
+#define SERVER_PATH "server_files/"
+#define CLIENT_PATH "client_files/"
+#define MESSAGE_SIZE 8196
+#endif
+
